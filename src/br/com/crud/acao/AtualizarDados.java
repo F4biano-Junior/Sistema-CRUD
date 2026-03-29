b83e991a2054cd40f8176879430ed3d81ab14fa1
F4314N0-JUN10R <f4bianojunior091101@gmail.com>
2026-03-28 22:04:25 -0300

    Atualizar

--- src/br/com/crud/acao/AtualizarDados.java
+++ src/br/com/crud/acao/AtualizarDados.java
@@ -13,6 +13,11 @@ public class AtualizarDados {
         for(ListaDePessoas p : listaDePessoas){
             System.out.println(p);
 
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("Atualizando dados de pessoas");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        for(Pessoa pessoa : pessoas.listar()){
+            System.out.println(pessoa);
         }
 
 
