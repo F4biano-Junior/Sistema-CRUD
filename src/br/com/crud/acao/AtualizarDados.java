db7ef885c5c6813bc4c0ed2802ae8a5552d461e3
F4314N0-JUN10R <f4bianojunior091101@gmail.com>
2026-04-08 18:26:01 -0300

    refactor: mudança completa na classe AtualizarDados

--- src/br/com/crud/acao/AtualizarDados.java
+++ src/br/com/crud/acao/AtualizarDados.java
@@ -1,6 +1,7 @@
 package br.com.crud.acao;
 
 import br.com.crud.cliente.Pessoa;
+import br.com.crud.exception.ValidarNomeException;
 import br.com.crud.repository.ListaDePessoas;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 
 public class AtualizarDados {
     public void atualizarDados(ListaDePessoas atualizarPessoa){
+
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         Scanner input = new Scanner(System.in);
@@ -16,41 +18,50 @@ public class AtualizarDados {
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         System.out.println("Atualizando dados de pessoas");
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        System.out.println(atualizarPessoa);
+        System.out.println(atualizarPessoa.listar());
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         System.out.println("Digite o ID de quem vai atualizar os dados:");
-        int inputId = input.nextInt();
-        input.nextLine();
+        try {
+            int inputId = input.nextInt();
+            input.nextLine();
 
-        Pessoa atualizarPorId = atualizarPessoa.buscarPorId(inputId);
+            Pessoa atualizarPorId = atualizarPessoa.buscarPorId(inputId);
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        System.out.println("Qual dado vai atualizar");
-        System.out.println("""
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            System.out.println("Qual dado vai atualizar");
+            System.out.println("""
                     1 - Nome
                     2 - Cidade
                     3 - Data De Nascimento
-                    """);
-        int opcao = input.nextInt();
-        input.nextLine();
+                   """);
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        switch (opcao) {
-            case 1:
-                System.out.println("Qual será o novo nome?");
-                String novoNome = input.nextLine();
-                atualizarPorId.atualizarNome(novoNome);
-                break;
-            case 2:
-                System.out.println("Qual será a nova cidade?");
-                String novaCidade = input.nextLine();
-                atualizarPorId.atualizarCidade(novaCidade);
-                break;
-            case 3:
-                System.out.println("Qual será a nova data de nascimento?");
-                LocalDate novaData = LocalDate.parse(input.next(), formato);
-                atualizarPorId.atualizarDataDeNascimento(novaData);
+            int opcao = input.nextInt();
+            input.nextLine();
+
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            switch (opcao) {
+                case 1:
+                    System.out.println("Qual será o novo nome?");
+                    try {
+                    String novoNome = input.nextLine();
+                        atualizarPorId.atualizarNome(novoNome);
+                        break;
+                    }catch (ValidarNomeException vNome){
+                        System.out.println(vNome.getMessage());
+                    }
+                case 2:
+                    System.out.println("Qual será a nova cidade?");
+                    String novaCidade = input.nextLine();
+                    atualizarPorId.atualizarCidade(novaCidade);
+                    break;
+                case 3:
+                    System.out.println("Qual será a nova data de nascimento?");
+                    LocalDate novaData = LocalDate.parse(input.next(), formato);
+                    atualizarPorId.atualizarDataDeNascimento(novaData);
+
+            }
+        }catch (IllegalArgumentException argumentException){
+            System.out.println("Por favor, digite uma opção válida!");
         }
-    }
+    }}
 
-}
